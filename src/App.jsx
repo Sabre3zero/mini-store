93296250a008348373d1bc77cd68b5ca2bda1722
Sabre3zero <108396167+Sabre3zero@users.noreply.github.com>
2026-04-29 12:47:39 +0300

    task1 update

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,22 @@ export default function App() {
   return (
     <>
       <h1 className="header">MiniStore</h1>
-      <h2>Приложения</h2>
+      <main>
+        <section className="card">
+          <h2>Product 1</h2>
+          <p>Text</p>
+        </section>
 
-      <div className="Textblock">
-        <h3>Приложение 1</h3>
-        <p>супер пупер мое приложение</p>
-      </div>
+        <section className="card">
+          <h2>Product 2</h2>
+          <p>Text</p>
+        </section>
 
-      <div className="Textblock">
-        <h3>Приложение 2</h3>
-        <p>супер пупер мое приложение</p>
-      </div>
-
-      <div className="Textblock">
-        <h3>Приложение 2</h3>
-        <p>супер пупер мое приложение</p>
-      </div>
-
-      <p>О нашем сайте.</p>
-      <p>asass</p>
+        <section className="card">
+          <h2>Product 3</h2>
+          <p>Text</p>
+        </section>
+      </main>
     </>
   );
 }
