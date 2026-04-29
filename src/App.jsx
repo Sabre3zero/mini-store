280eb11ee7d5fe05830f40fea93da597d34d4bb6
Sabre3zero <108396167+Sabre3zero@users.noreply.github.com>
2026-04-29 13:03:39 +0300

    task1 style usage transfer to 'module'

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
+import styles from './App.module.css'
+
+
 export default function App() {
   return (
     <>
-      <h1 className="header">MiniStore</h1>
+      <h1 className={styles.header}>MiniStore</h1>
       <main>
-        <section className="card">
+        <section className={styles.card}>
           <h2>Product 1</h2>
           <p>Text</p>
+          <span>27/04</span>
         </section>
 
-        <section className="card">
+        <section className={styles.card}>
           <h2>Product 2</h2>
           <p>Text</p>
+          <span>28/04</span>
         </section>
 
-        <section className="card">
+        <section className={styles.card}>
           <h2>Product 3</h2>
           <p>Text</p>
+          <span>2/04</span>
         </section>
       </main>
     </>
