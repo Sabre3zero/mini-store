2d114fcbd2bb5ac41a522908ec79f4d5a8ac7cf1
Sabre3zero <108396167+Sabre3zero@users.noreply.github.com>
2026-05-07 02:47:43 +0300

    Search by exact name and tag added + checkbox for selecting products with free tag (can be done via search as well)

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,73 @@
-import './variables.css'
+import './style_variables.css'
 import styles from './App.module.css'
 import {AppCard} from './AppCard'
+import {apps} from './variables'
+import {useEffect, useState} from 'react'
 
 
 export default function App() {
+  
+  let [text, setText] = useState('')
+  let [selectedApps, setSelectedApps] = useState(apps)
+
+  let [check, setCheck] = useState(false)
+
+  useEffect(() => {
+
+    let filtered = apps
+    
+    if (text !== ''){
+      filtered = filtered.filter(app => {
+        return app.title === text || app.tags.some(tag => tag === text)
+      })
+    }
+    else {
+      setSelectedApps(apps)
+    }
+
+    if (check){
+      filtered = filtered.filter(app => {
+        return app.tags.includes('free')
+      })
+    }
+
+    setSelectedApps(filtered)
+
+  }, [text, check]);
+
+  let inputHandler = function (event) {
+    let text = event.currentTarget.value
+    setText(text)
+
+  }
+
+  let checkHandler = function (event) {
+    let check = event.currentTarget.checked
+    setCheck(check)
+  }
+
+
   return (
     <>
-      <body>
-        <header>
-          <h1 className={styles.header}>MiniStore</h1>
-        </header>
+      <header>
+        <h1 className={styles.header}>MiniStore</h1>
+      </header>
 
-        <main>
-          {[
-            {image: '/Product1.png', title: 'Product1', text: 'Some very cool description about Product1, also it is very very very very very very very long. Yeah it is definetely helpul huh? U rly still reading this? You are better doing... Oh, cutoff, see ya! man rly? why are you reading this XD Go get some sleep', tags: ['tag1', 'tag2']},
-            {image: '/Product2.png', title: 'Product2', text: 'Text2', tags: ['tag1', 'tag2', 'tag3', 'looong tag']},
-            {image: '/Product1.png', title: 'Product3', text: 'Text3', tags: ['tg', 'tag4']},
-            {image: '/Product1.png', title: 'Product4', text: 'Text4', tags: ['tag5']},
-            {image: '/Product1.png', title: 'Product5', text: 'Text5', tags: ['tag6', 'tag7', 'tag8', 'tag9', 'tag10']},
-            {image: '/Product1.png', title: 'Product6', text: 'Text6', tags: ['tag11', 'tag12', 'tag13']},
-            {image: '/Product1.png', title: 'Product7', text: 'Text7', tags: ['tag14', 'tag15']},
-            {image: '/Product1.png', title: 'Product8', text: 'Text8', tags: ['tag16']},
-            {image: '/Product1.png', title: 'Product9', text: 'Text9', tags: ['tag17', 'tag18', 'tag19', 'tag20']}
-          ].map(({image, title, text, tags}) => (
-          <AppCard
-            image={image}
-            title={title}
-            text={text}
-            tags={tags}
-          />
-          ))}
-        </main>
-      </body>
+      <div>
+        <input type='text' onInput={inputHandler}/>
+        <input type='checkbox' onChange={checkHandler}/>
+      </div>
+
+      <main>
+        {selectedApps.map(({image, title, text, tags}) => (
+        <AppCard
+          image={image}
+          title={title}
+          text={text}
+          tags={tags}
+        />
+        ))}
+      </main>
     </>
   );
 }
---
+++ src/variables.jsx
@@ -0,0 +1,11 @@
+export const apps = [
+    {image: '/Product1.png', title: 'Product1', text: 'Some very cool description about Product1, also it is very very very very very very very long. Yeah it is definetely helpul huh? U rly still reading this? You are better doing... Oh, cutoff, see ya! man rly? why are you reading this XD Go get some sleep', tags: ['free', 'tag1']},
+    {image: '/Product2.png', title: 'Product2', text: 'Text2', tags: ['free', 'tag1', 'tag2', 'tag3']},
+    {image: '/Product1.png', title: 'Product3', text: 'Text3', tags: ['tag4']},
+    {image: '/Product1.png', title: 'Product4', text: 'Text4', tags: ['free']},
+    {image: '/Product1.png', title: 'Product5', text: 'Text5', tags: ['tag1', 'tag2', 'tag3', 'tag4', 'tag5']},
+    {image: '/Product1.png', title: 'Product6', text: 'Text6', tags: ['tag2', 'tag3', 'tag5']},
+    {image: '/Product1.png', title: 'Product7', text: 'Text7', tags: ['free', 'tag5']},
+    {image: '/Product1.png', title: 'Product8', text: 'Text8', tags: ['tag6']},
+    {image: '/Product1.png', title: 'Product9', text: 'Text9', tags: ['free', 'tag1', 'tag2', 'tag6']}
+]
